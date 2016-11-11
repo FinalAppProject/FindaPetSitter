c81c3e4c69253b8252a7e6e2b2587aa24d4d993f
Mugdha Khade <mugdha.khade@gmail.com>
2016-11-10 23:08:19 -0800

    ok

---
+++ app/src/main/java/org/finalappproject/findapetsitter/fragments/MyPetsFragment.java
@@ -0,0 +1,11 @@
+package org.finalappproject.findapetsitter.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by mkhade on 11/9/2016.
+ */
+
+public class MyPetsFragment extends Fragment {
+
+}
