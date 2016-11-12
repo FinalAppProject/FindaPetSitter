e3a236a6af42b41a234d7524d5e739d37b328f89
Unknown <eduardo_thp@yahoo.com.br>
2016-11-12 00:32:20 -0800

    Initial Model Implementation
    Add/Detail classes: User, Pet, Address, EmergencyContact, PetTypes
    Register Parse object classes in the application class

--- app/src/main/java/org/finalappproject/findapetsitter/model/Pet.java
+++ app/src/main/java/org/finalappproject/findapetsitter/model/Pet.java
@@ -1,8 +1,74 @@
 package org.finalappproject.findapetsitter.model;
 
-/**
- * Created by eddie_thp on 11/8/16.
- */
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
 
-public class Pet {
+/**
+ * User pet
+ */
+@ParseClassName("Pet")
+public class Pet extends ParseObject {
+    private static final String KEY_TYPE = "type";
+    private static final String KEY_BREED = "breed";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_PROFILE_IMAGE = "profileImage";
+    private static final String KEY_DESCRIPTION = "description";
+    private static final String KEY_SPECIAL_NEEDS = "specialNeeds";
+    private static final String KEY_EMERGENCY_CONTANT = "emergencyContact";
+
+    public PetTypes getType() {
+        return PetTypes.valueOf(getString(KEY_TYPE));
+    }
+
+    public void setType(PetTypes type) {
+        put(KEY_TYPE, type.name());
+    }
+
+    public String getBreed() {
+        return getString(KEY_BREED);
+    }
+
+    public void setBreed(String breed) {
+        put(KEY_BREED, breed);
+    }
+
+    public String getName() {
+        return getString(KEY_NAME);
+    }
+
+    public void setName(String name) {
+        put(KEY_NAME, name);
+    }
+
+    public String getProfileImage() {
+        return getString(KEY_PROFILE_IMAGE);
+    }
+
+    public void setProfileImage(String profileImage) {
+        put(KEY_PROFILE_IMAGE, profileImage);
+    }
+
+    public String getDescription() {
+        return getString(KEY_DESCRIPTION);
+    }
+
+    public void setDescription(String description) {
+        put(KEY_DESCRIPTION, description);
+    }
+
+    public String getSpecialNeeds() {
+        return getString(KEY_SPECIAL_NEEDS);
+    }
+
+    public void setSpecialNeeds(String specialNeeds) {
+        put(KEY_SPECIAL_NEEDS, specialNeeds);
+    }
+
+    public EmergencyContact getEmergencyContact() {
+        return (EmergencyContact) getParseObject(KEY_EMERGENCY_CONTANT);
+    }
+
+    public void setEmergencyContact(EmergencyContact emergencyContact) {
+        put(KEY_EMERGENCY_CONTANT, emergencyContact);
+    }
 }
--- app/src/main/java/org/finalappproject/findapetsitter/model/User.java
+++ app/src/main/java/org/finalappproject/findapetsitter/model/User.java
@@ -3,6 +3,8 @@ package org.finalappproject.findapetsitter.model;
 import com.parse.ParseClassName;
 import com.parse.ParseUser;
 
+import java.util.List;
+
 /**
  * Custom ParseUser implementation
  */
@@ -10,6 +12,20 @@ import com.parse.ParseUser;
 public class User extends ParseUser {
 
     private static final String KEY_FULL_NAME = "fullName";
+
+    private static final String KEY_NICK_NAME = "nickName";
+
+    private static final String KEY_PROFILE_IMAGE = "profileImage";
+
+    private static final String KEY_DESCRIPTION = "description";
+
+    private static final String KEY_ADDRESS = "address";
+
+    private static final String KEY_PHONE = "phone";
+
+    private static final String KEY_PETS = "pets";
+
+    private static final String KEY_PET_SITTER = "petSitter";
 
     public void setFullName(String fullName) {
         put(KEY_FULL_NAME, fullName);
@@ -19,4 +35,59 @@ public class User extends ParseUser {
         return getString(KEY_FULL_NAME);
     }
 
+    public void setNickName(String nickName) {
+        put(KEY_NICK_NAME, nickName);
+    }
+
+    public String getNickName() {
+        return getString(KEY_NICK_NAME);
+    }
+
+    public String getProfileImage() {
+        return getString(KEY_PROFILE_IMAGE);
+    }
+
+    public void setProfileImage(String profileImage) {
+        put(KEY_PROFILE_IMAGE, profileImage);
+    }
+
+    public String getDescription() {
+        return getString(KEY_DESCRIPTION);
+    }
+
+    public void setDescription(String description) {
+        put(KEY_DESCRIPTION, description);
+    }
+
+    public Address getAddress() {
+        return (Address) getParseObject(KEY_ADDRESS);
+    }
+
+    public void setAddress(Address address) {
+        put(KEY_ADDRESS, address);
+    }
+
+    public String getPhone() {
+        return getString(KEY_PHONE);
+    }
+
+    public void setPhone(String phone) {
+        put(KEY_PHONE, phone);
+    }
+
+    public List<Pet> getPets() {
+        return getList(KEY_PETS);
+    }
+
+    public void setPets(List<Pet> pets) {
+        put(KEY_PETS, pets);
+    }
+
+    public boolean isPetSitter() {
+        return getBoolean(KEY_PET_SITTER);
+    }
+
+    public void setPetSitter(boolean petSitter) {
+        put(KEY_PET_SITTER, petSitter);
+    }
 }
