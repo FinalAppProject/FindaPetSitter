a5a4e9672cb3964a6ca0c1b0cc74059b1df33336
Unknown <eduardo_thp@yahoo.com.br>
2016-11-12 11:50:59 -0800

    Started User Profile / Pet Profile work
    - created user profile and pet profile activites
    - added button to launch user profile activity
    - modified address model

---
+++ app/src/main/java/org/finalappproject/findapetsitter/activities/UserProfileActivity.java
@@ -0,0 +1,70 @@
+package org.finalappproject.findapetsitter.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import org.finalappproject.findapetsitter.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * User profile activity
+ */
+public class UserProfileActivity extends AppCompatActivity {
+
+    // TODO Bind other views
+
+    @BindView(R.id.btAddPet)
+    Button btAddPet;
+
+    @BindView(R.id.btSave)
+    Button btSave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+
+        // Bind views
+        ButterKnife.bind(this);
+        // Setup add pet button
+        setupAddPetButton();
+        // Setup save button
+        setupSaveButton();
+    }
+
+    private void setupAddPetButton() {
+        btAddPet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startPetProfileActivity();
+            }
+        });
+    }
+
+    private void setupSaveButton() {
+        btAddPet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO save UserProfile parse object, which will contain the pets information
+
+            }
+        });
+    }
+
+    /**
+     * Starts the pet profile activity
+     */
+    private void startPetProfileActivity() {
+        Intent petProfileIntent = new Intent(this, PetProfileActivity.class);
+        startActivity(petProfileIntent);
+
+        // TODO when the pet profile activity returns, get the pet parse object
+        // TODO add the pet parse object to the user object
+        // TODO add the pet parse object to the list of pets and display in the recycler view
+    }
+}
