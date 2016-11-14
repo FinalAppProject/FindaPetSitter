c38424a0535f675c648e700032619063bf625543
Unknown <eduardo_thp@yahoo.com.br>
2016-11-13 19:22:28 -0800

    Finish User and Pet Profile activities
    implement ability to edit user profile
    add profile image
    add and modify pets

--- app/src/main/java/org/finalappproject/findapetsitter/activities/PetProfileActivity.java
+++ app/src/main/java/org/finalappproject/findapetsitter/activities/PetProfileActivity.java
@@ -1,15 +1,270 @@
 package org.finalappproject.findapetsitter.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.TransitionDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import org.finalappproject.findapetsitter.R;
+import org.finalappproject.findapetsitter.application.AppConstants;
+import org.finalappproject.findapetsitter.model.Pet;
+import org.finalappproject.findapetsitter.model.PetType;
+import org.finalappproject.findapetsitter.model.User;
+import org.finalappproject.findapetsitter.util.ImageHelper;
 
-public class PetProfileActivity extends AppCompatActivity {
+import java.io.IOException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Pet profile activity allows the user to add/modify its pets and visualize others users pets
+ */
+public class PetProfileActivity extends AppCompatActivity implements GetCallback<Pet>, SaveCallback {
+
+    private static final String LOG_TAG = "PetProfileActivity";
+
+    public static final int REQUEST_CODE_ADD_PET = 10;
+
+    public static final int REQUEST_CODE_EDIT_PET = 20;
+
+    public static final int RESULT_CODE_SAVE_SUCCESS = 10;
+
+    public static final int RESULT_CODE_SAVE_FAILURE = 11;
+
+    public static final String EXTRA_PET_POSITION = "EXTRA_PET_POSITION";
+
+    public static final String EXTRA_PET_OBJECT_ID = "EXTRA_PET_OBJECT_ID";
+
+
+    @BindView(R.id.ivProfileImage)
+    ImageView ivProfileImage;
+
+    @BindView(R.id.etName)
+    EditText etName;
+
+    @BindView(R.id.etDescription)
+    EditText etDescription;
+
+    @BindView(R.id.rgType)
+    RadioGroup rgType;
+
+    @BindView(R.id.rbDog)
+    RadioButton rbDog;
+
+    @BindView(R.id.rbCat)
+    RadioButton rbCat;
+
+    @BindView(R.id.etBreed)
+    EditText etBreed;
+
+    @BindView(R.id.etSpecialNeeds)
+    EditText etSpecialNeeds;
+
+    @BindView(R.id.btSave)
+    Button btSave;
+
+    int mPosition;
+    Pet mPet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet_profile);
+        // Bind views
+        ButterKnife.bind(this);
+        // Retrieve extra
+        retrievePetExtra();
+        // Setup views
+        setupProfileImage();
+        setupSaveButton();
+    }
+
+    void setupProfileImage() {
+        ivProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageHelper.startImagePickerActivity(PetProfileActivity.this);
+            }
+        });
+    }
+
+    void setupSaveButton() {
+        btSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                savePet();
+            }
+        });
+    }
+
+    void retrievePetExtra() {
+        mPosition = -1;
+        mPet = null;
+        Intent petProfileIntent = getIntent();
+
+        String petObjectId = petProfileIntent.getStringExtra(EXTRA_PET_OBJECT_ID);
+        if (petObjectId != null && !petObjectId.isEmpty()) {
+            mPosition = petProfileIntent.getIntExtra(EXTRA_PET_POSITION, -1);
+            loadPet(petObjectId);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case AppConstants.CODE_IMAGE_PICKER:
+                setProfileImage(data);
+                break;
+        }
+    }
+
+    void loadPet(String objectId) {
+        ParseQuery<Pet> query = ParseQuery.getQuery(Pet.class);
+        // TODO verify/validate cache policy
+        query.setCachePolicy(ParseQuery.CachePolicy.NETWORK_ELSE_CACHE);
+        query.getInBackground(objectId, this);
+    }
+
+    @Override
+    public void done(Pet pet, ParseException e) {
+        if (e == null) {
+            mPet = pet;
+
+            // Profile Image
+            ImageHelper.loadImage(this, pet.getProfileImage(), R.drawable.cat, ivProfileImage);
+
+            // Pet Type
+            PetType type = pet.getType();
+            if (type != null) {
+                switch (type) {
+                    case Dog:
+                        rgType.check(R.id.rbDog);
+                        break;
+                    case Cat:
+                        rgType.check(R.id.rbCat);
+                        break;
+                }
+            }
+            pet.setType(type);
+            // Remaining attributes
+            etName.setText(pet.getName());
+            etDescription.setText(etDescription.getText());
+            etBreed.setText(pet.getBreed());
+            etSpecialNeeds.setText(pet.getSpecialNeeds());
+
+        } else {
+            Log.e(LOG_TAG, "Failed to load pet", e);
+            Toast.makeText(this, "Failed to load pet information", Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
+    void setProfileImage(Intent data) {
+        if (data != null) {
+            // asBitmap is required to force the use of a BitmapDrawable into the ImageView
+            // This may eventually fail e.g. Animated GIFs
+            // for more info: https://github.com/bumptech/glide/issues/1083
+            // TODO look for a better solution
+            Glide.with(this).fromUri().asBitmap().load(data.getData()).placeholder(R.drawable.cat).into(ivProfileImage);
+
+/*
+            try {
+                Uri photoUri = data.getData();
+                Bitmap selectedImage = MediaStore.Images.Media.get(this.getContentResolver(), photoUri);
+                ivProfileImage.setImageBitmap(selectedImage);
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Failed to set profile image", e);
+                Toast.makeText(this, "Failed to set profile image", Toast.LENGTH_LONG).show();
+            }
+        */
+        }
+    }
+
+    void savePet() {
+        boolean newPet = false;
+        Pet pet = mPet;
+
+        if (pet == null) {
+            newPet = true;
+            pet = new Pet();
+            mPet = pet;
+        }
+
+        // Profile Image
+        Drawable profileImageDrawable = ivProfileImage.getDrawable();
+
+        if (profileImageDrawable != null) {
+            if (profileImageDrawable instanceof BitmapDrawable) {
+                Bitmap profileBitmap = ((BitmapDrawable) profileImageDrawable).getBitmap();
+                ParseFile imageFile = ImageHelper.createParseFile(pet.getObjectId(), profileBitmap);
+                pet.setProfileImage(imageFile);
+            }
+            // TODO handle failure to retrieve profile image from drawable
+        }
+
+        // Pet Type
+        PetType type = null;
+        switch (rgType.getCheckedRadioButtonId()) {
+            case R.id.rbDog:
+                type = PetType.Dog;
+                break;
+            case R.id.rbCat:
+                type = PetType.Cat;
+                break;
+        }
+        pet.setType(type);
+
+        // Remaining attributes
+        pet.setName(etName.getText().toString());
+        pet.setDescription(etDescription.getText().toString());
+        pet.setBreed(etBreed.getText().toString());
+        pet.setSpecialNeeds(etSpecialNeeds.getText().toString());
+
+        // TODO pet.setEmergencyContact();
+
+        if (newPet) {
+            User appUser = (User) ParseUser.getCurrentUser();
+            appUser.addPet(pet, this);
+        } else {
+            pet.saveInBackground(this);
+        }
+    }
+
+    @Override
+    public void done(ParseException e) {
+        if (e == null) {
+            Intent resultIntent = new Intent();
+            resultIntent.putExtra(EXTRA_PET_POSITION, mPosition);
+            resultIntent.putExtra(EXTRA_PET_OBJECT_ID, mPet.getObjectId());
+            setResult(RESULT_CODE_SAVE_SUCCESS, resultIntent);
+        } else {
+            Log.e(LOG_TAG, "Failed to save pet", e);
+            setResult(RESULT_CODE_SAVE_FAILURE);
+        }
+        finish();
     }
 }
--- app/src/main/java/org/finalappproject/findapetsitter/activities/UserProfileActivity.java
+++ app/src/main/java/org/finalappproject/findapetsitter/activities/UserProfileActivity.java
@@ -1,22 +1,93 @@
 package org.finalappproject.findapetsitter.activities;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.SaveCallback;
 
 import org.finalappproject.findapetsitter.R;
+import org.finalappproject.findapetsitter.adapters.PetsAdapter;
+import org.finalappproject.findapetsitter.application.AppConstants;
+import org.finalappproject.findapetsitter.model.Address;
+import org.finalappproject.findapetsitter.model.Pet;
+import org.finalappproject.findapetsitter.model.User;
+import org.finalappproject.findapetsitter.util.ImageHelper;
+import org.finalappproject.findapetsitter.util.recyclerview.ItemClickSupport;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+import static android.R.attr.data;
+import static org.finalappproject.findapetsitter.activities.PetProfileActivity.EXTRA_PET_OBJECT_ID;
+import static org.finalappproject.findapetsitter.activities.PetProfileActivity.EXTRA_PET_POSITION;
+import static org.finalappproject.findapetsitter.activities.PetProfileActivity.REQUEST_CODE_ADD_PET;
+import static org.finalappproject.findapetsitter.activities.PetProfileActivity.REQUEST_CODE_EDIT_PET;
+import static org.finalappproject.findapetsitter.activities.PetProfileActivity.RESULT_CODE_SAVE_FAILURE;
+import static org.finalappproject.findapetsitter.activities.PetProfileActivity.RESULT_CODE_SAVE_SUCCESS;
+
 /**
  * User profile activity
  */
-public class UserProfileActivity extends AppCompatActivity {
+public class UserProfileActivity extends AppCompatActivity implements SaveCallback {
 
-    // TODO Bind other views
+    private static final String LOG_TAG = "UserProfileActivity";
+
+    public static final String EXTRA_USER_OBJECT_ID = "EXTRA_USER_OBJECT_ID";
+
+    @BindView(R.id.ivProfileImage)
+    ImageView ivProfileImage;
+
+    @BindView(R.id.etFullName)
+    EditText etFullName;
+
+    @BindView(R.id.etNickName)
+    EditText etNickName;
+
+    @BindView(R.id.etDescription)
+    EditText etDescription;
+
+    @BindView(R.id.etPhoneNumber)
+    EditText etPhoneNumber;
+
+    @BindView(R.id.etAddress)
+    EditText etAddress;
+
+    @BindView(R.id.etZipCode)
+    EditText etZipCode;
+
+    @BindView(R.id.etCity)
+    EditText etCity;
+
+    @BindView(R.id.etState)
+    EditText etState;
+
+    @BindView(R.id.rvPets)
+    RecyclerView rvPets;
 
     @BindView(R.id.btAddPet)
     Button btAddPet;
@@ -24,18 +95,51 @@ public class UserProfileActivity extends AppCompatActivity {
     @BindView(R.id.btSave)
     Button btSave;
 
+    User mUser;
+
+    List<Pet> mPets;
+
+    PetsAdapter mPetsAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
-
         // Bind views
         ButterKnife.bind(this);
-        // Setup add pet button
+        retrieveUserExtra();
+        setupProfileImage();
         setupAddPetButton();
-        // Setup save button
         setupSaveButton();
+        setupPetsRecyclerView();
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case AppConstants.CODE_IMAGE_PICKER:
+                setProfileImage(data);
+                break;
+            case REQUEST_CODE_EDIT_PET:
+                onPetEdited(resultCode, data);
+                break;
+            case REQUEST_CODE_ADD_PET:
+                onPetAdded(resultCode, data);
+                break;
+        }
+    }
+
+    void setupProfileImage() {
+        ivProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageHelper.startImagePickerActivity(UserProfileActivity.this);
+            }
+        });
+    }
+
 
     private void setupAddPetButton() {
         btAddPet.setOnClickListener(new View.OnClickListener() {
@@ -47,24 +151,186 @@ public class UserProfileActivity extends AppCompatActivity {
     }
 
     private void setupSaveButton() {
-        btAddPet.setOnClickListener(new View.OnClickListener() {
+        btSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO save UserProfile parse object, which will contain the pets information
-
+                saveUser();
             }
         });
+    }
+
+    private void setupPetsRecyclerView() {
+        mPets = new ArrayList<>(mUser.getPets());
+        mPetsAdapter = new PetsAdapter(this, mPets);
+        rvPets.setAdapter(mPetsAdapter);
+        LinearLayoutManager linerLayoutManager = new LinearLayoutManager(this);
+        linerLayoutManager.setOrientation(LinearLayout.HORIZONTAL);
+        rvPets.setLayoutManager(linerLayoutManager);
+
+        ItemClickSupport.addTo(rvPets).setOnItemClickListener(
+                new ItemClickSupport.OnItemClickListener() {
+                    @Override
+                    public void onItemClicked(RecyclerView recyclerView, int position, View v) {
+                        startPetProfileActivity(position);
+                    }
+                }
+        );
+    }
+
+    void retrieveUserExtra() {
+        mUser = null;
+        Intent userProfileIntent = getIntent();
+
+        String userObjectId = userProfileIntent.getStringExtra(EXTRA_USER_OBJECT_ID);
+        if (userObjectId != null && userObjectId.isEmpty()) {
+            // TODO loadUser(userObjectId);
+        } else {
+            mUser = (User) User.getCurrentUser();
+        }
+
+        loadUser();
+    }
+
+    void loadUser() {
+
+        ImageHelper.loadImage(this, mUser.getProfileImage(), R.drawable.account_plus, ivProfileImage);
+
+        etFullName.setText(mUser.getFullName());
+        etNickName.setText(mUser.getNickName());
+        etDescription.setText(mUser.getDescription());
+        etPhoneNumber.setText(mUser.getPhone());
+
+        Address userAddress = mUser.getAddress();
+        if (userAddress != null) {
+            etAddress.setText(userAddress.getAddress());
+            etZipCode.setText(userAddress.getZipCode());
+            etCity.setText(userAddress.getCity());
+            etState.setText(userAddress.getState());
+        }
+
+    }
+
+    void setProfileImage(Intent data) {
+        if (data != null) {
+            Glide.with(this).fromUri().asBitmap().load(data.getData()).placeholder(R.drawable.account_plus).into(ivProfileImage);
+        }
     }
 
     /**
      * Starts the pet profile activity
      */
+    private void startPetProfileActivity(int position) {
+        Pet pet = mPets.get(position);
+        Intent petProfileIntent = new Intent(this, PetProfileActivity.class);
+        petProfileIntent.putExtra(EXTRA_PET_POSITION, position);
+        petProfileIntent.putExtra(EXTRA_PET_OBJECT_ID, pet.getObjectId());
+        startActivityForResult(petProfileIntent, REQUEST_CODE_EDIT_PET);
+    }
+
     private void startPetProfileActivity() {
         Intent petProfileIntent = new Intent(this, PetProfileActivity.class);
-        startActivity(petProfileIntent);
+        startActivityForResult(petProfileIntent, REQUEST_CODE_ADD_PET);
 
         // TODO when the pet profile activity returns, get the pet parse object
         // TODO add the pet parse object to the user object
         // TODO add the pet parse object to the list of pets and display in the recycler view
+    }
+
+    void onPetAdded(int resultCode, Intent data) {
+        if (resultCode == PetProfileActivity.RESULT_CODE_SAVE_SUCCESS) {
+            String petObjectId = data.getStringExtra(EXTRA_PET_OBJECT_ID);
+
+            ParseQuery<Pet> query = ParseQuery.getQuery(Pet.class);
+            // TODO verify/validate cache policy
+            query.setCachePolicy(ParseQuery.CachePolicy.CACHE_ELSE_NETWORK);
+            query.getInBackground(petObjectId, new GetCallback<Pet>() {
+                @Override
+                public void done(Pet pet, ParseException e) {
+                    if (e == null) {
+                        addPet(pet);
+                    } else {
+                        Log.e(LOG_TAG, "Failed to load pet", e);
+                        Toast.makeText(UserProfileActivity.this, "Failed to fetch added pet", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        } else if (resultCode == RESULT_CODE_SAVE_FAILURE) {
+            Toast.makeText(this, "Failed to add pet", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    void addPet(Pet pet) {
+        int size = mPets.size();
+        mPets.add(pet);
+        mPetsAdapter.notifyItemInserted(size);
+    }
+
+    void onPetEdited(int resultCode, Intent data) {
+        if (resultCode == PetProfileActivity.RESULT_CODE_SAVE_SUCCESS) {
+            final int position = data.getIntExtra(EXTRA_PET_POSITION, -1);
+
+            if (position != -1) {
+                Pet pet = mPets.get(position);
+                pet.fetchInBackground(new GetCallback<ParseObject>() {
+                    @Override
+                    public void done(ParseObject object, ParseException e) {
+                        if (e == null) {
+                            mPetsAdapter.notifyItemChanged(position);
+                        } else {
+                            Log.e(LOG_TAG, "Failed to fetch edited pet", e);
+                            Toast.makeText(UserProfileActivity.this, "Failed to fetch pet changes", Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                });
+
+            }
+        } else if (resultCode == RESULT_CODE_SAVE_FAILURE) {
+            Toast.makeText(this, "Failed to edit pet", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    void saveUser() {
+
+        // Profile Image
+        Drawable profileImageDrawable = ivProfileImage.getDrawable();
+
+        if (profileImageDrawable != null) {
+            if (profileImageDrawable instanceof BitmapDrawable) {
+                Bitmap profileBitmap = ((BitmapDrawable) profileImageDrawable).getBitmap();
+                ParseFile imageFile = ImageHelper.createParseFile(mUser.getObjectId(), profileBitmap);
+                mUser.setProfileImage(imageFile);
+            }
+            // TODO handle failure to retrieve profile image from drawable
+        }
+
+        mUser.setFullName(etFullName.getText().toString());
+        mUser.setNickName(etNickName.getText().toString());
+        mUser.setDescription(etDescription.getText().toString());
+        mUser.setPhone(etPhoneNumber.getText().toString());
+
+        // User address
+        Address userAddress = mUser.getAddress();
+        if (userAddress == null) {
+            userAddress = new Address();
+        }
+        userAddress.setAddress(etAddress.getText().toString());
+        userAddress.setZipCode(etZipCode.getText().toString());
+        userAddress.setCity(etCity.getText().toString());
+        userAddress.setState(etState.getText().toString());
+
+        mUser.saveInBackground(this);
+    }
+
+    @Override
+    public void done(ParseException e) {
+        if (e == null) {
+            // TODO return result ? see pet profile
+        } else {
+            Log.e(LOG_TAG, "Failed to save user", e);
+            // TODO return result ?
+            // setResult(RESULT_CODE_SAVE_FAILURE);
+        }
+        finish();
     }
 }
