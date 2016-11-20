36c8f38298d96effd2f986da3b208e08ab859f36
Unknown <eduardo_thp@yahoo.com.br>
2016-11-19 20:26:24 -0800

    Some imports cleanup

--- app/src/main/java/org/finalappproject/findapetsitter/activities/HomeActivity.java
+++ app/src/main/java/org/finalappproject/findapetsitter/activities/HomeActivity.java
@@ -3,44 +3,25 @@ package org.finalappproject.findapetsitter.activities;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
-
-import com.astuetz.PagerSlidingTabStrip;
-import com.parse.ParseUser;
 
 import org.finalappproject.findapetsitter.R;
-import org.finalappproject.findapetsitter.adapters.HomePagerAdapter;
-import org.finalappproject.findapetsitter.fragments.AvailableSittersFragment;
-import org.finalappproject.findapetsitter.fragments.FavoriteSittersFragment;
 import org.finalappproject.findapetsitter.fragments.FilterFragment;
-import org.finalappproject.findapetsitter.fragments.MyPetsFragment;
 import org.finalappproject.findapetsitter.fragments.PetOwnerHomeFragment;
 import org.finalappproject.findapetsitter.fragments.SitterHomeFragment;
+import org.finalappproject.findapetsitter.fragments.UserProfileFragment;
 import org.finalappproject.findapetsitter.model.User;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-
-import static android.R.attr.data;
-import static android.R.attr.fragment;
-import static android.R.attr.orientation;
-import static org.finalappproject.findapetsitter.R.id.vpPager;
-
-import static com.parse.ParseUser.getCurrentUser;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -258,8 +239,8 @@ public class HomeActivity extends AppCompatActivity {
         }
 
         fm.beginTransaction()
-                    .add(R.id.flContent, fragment, tag)
-                    .commit();
+                .add(R.id.flContent, fragment, tag)
+                .commit();
 
         return fragment;
     }
