29c02a459f0fbfd6f5f3ae9039749276bde6d567
Aoi <aoimatsuba@gmail.com>
2016-11-20 22:42:38 -0800

    Respond tag implemented

--- app/src/main/java/org/finalappproject/findapetsitter/activities/ReceivedRequestActivity.java
+++ app/src/main/java/org/finalappproject/findapetsitter/activities/ReceivedRequestActivity.java
@@ -67,7 +67,7 @@ public class ReceivedRequestActivity extends AppCompatActivity implements GetCal
             @Override
             public void onClick(View view) {
                 mRequest.setStatus(REQUEST_ACCEPTED);
-                Integer status = mRequest.getStatus();
+                mRequest.saveInBackground();
             }
         });
 
@@ -75,6 +75,7 @@ public class ReceivedRequestActivity extends AppCompatActivity implements GetCal
             @Override
             public void onClick(View view) {
                 mRequest.setStatus(REQUEST_REJECTED);
+                mRequest.saveInBackground();
             }
         });
     }
