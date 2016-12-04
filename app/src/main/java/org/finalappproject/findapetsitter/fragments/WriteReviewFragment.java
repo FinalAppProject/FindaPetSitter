aebe69221638e84aabe5a53cc5decada2dcee8a0
Mugdha Khade <mugdha.khade@gmail.com>
2016-12-04 02:32:24 -0800

    Change layout of Reviews input and request input

--- app/src/main/java/org/finalappproject/findapetsitter/fragments/WriteReviewFragment.java
+++ app/src/main/java/org/finalappproject/findapetsitter/fragments/WriteReviewFragment.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -24,6 +25,7 @@ import org.finalappproject.findapetsitter.R;
 import org.finalappproject.findapetsitter.model.PetType;
 import org.finalappproject.findapetsitter.model.Review;
 import org.finalappproject.findapetsitter.model.User;
+import org.finalappproject.findapetsitter.util.ImageHelper;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -51,6 +53,9 @@ public class WriteReviewFragment extends DialogFragment implements GetCallback<U
 
     @BindView(R.id.rbReviewRating)
     RatingBar reviewRatingRatingBar;
+
+    @BindView(R.id.ivReviewImageView)
+    ImageView reviewImageView;
 
     private static final String LOG_TAG = "ReviewFragment";
     CalendarPickerDialogFragment calendarPickerDialogFragment;
@@ -110,6 +115,7 @@ public class WriteReviewFragment extends DialogFragment implements GetCallback<U
                 Toast.makeText(getActivity(),"Your Selected Ratings  : " + String.valueOf(rating),Toast.LENGTH_LONG).show();
             }
         });
+
         return vi;
     }
 
