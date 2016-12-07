33283371b1d36d4a7942bad7af26a1d42fe11f80
Mugdha Khade <mugdha.khade@gmail.com>
2016-12-06 22:06:04 -0800

    remove toasts, add tab coloring

--- app/src/main/java/org/finalappproject/findapetsitter/fragments/CalendarPickerDialogFragment.java
+++ app/src/main/java/org/finalappproject/findapetsitter/fragments/CalendarPickerDialogFragment.java
@@ -8,7 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.squareup.timessquare.CalendarPickerView;
 
@@ -54,8 +53,6 @@ public class CalendarPickerDialogFragment extends DialogFragment {
     void onNextClicked(){
         ArrayList<Date> selectedDates = (ArrayList<Date>)calendar
                 .getSelectedDates();
-        Toast.makeText(getActivity(), selectedDates.toString(),
-                Toast.LENGTH_LONG).show();
         onDatesSelectedListener.onDatesSelected(selectedDates);
         dismiss();
     }
